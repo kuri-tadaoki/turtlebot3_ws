b0ff8274611a9a2bc302c167c31201ed955c6c4c
kuri-tadaoki <e1x20901@oit.ac.jp>
2024-01-07 00:03:39 +0900

    ぬるぽ

--- chapter5/yolov5_ros2/yolov5_ros2/ore_object.py
+++ chapter5/yolov5_ros2/yolov5_ros2/ore_object.py
@@ -1,44 +1,35 @@
 import sys
+import time
 import subprocess
-import threading
-import tkinter as tk
 import cv2
 import numpy as np
-import time
-import asyncio
 import rclpy
 from rclpy.node import Node
-from rclpy import action
+from rclpy.action import ActionClient
 from sensor_msgs.msg import Image, CameraInfo
 from geometry_msgs.msg import TransformStamped
+from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
 from rclpy.utilities import remove_ros_args
-from message_filters import Subscriber, ApproximateTimeSynchronizer
 from tf2_ros import TransformBroadcaster
-from geometry_msgs.msg import Twist
 from yolov5_ros2.detector import Detector, parse_opt
-from playsound import playsound
-from rclpy.action import ActionClient
-from action_msgs.srv import CancelGoal
-from std_msgs.msg import String
 from nav2_msgs.action import NavigateToPose
+from message_filters import ApproximateTimeSynchronizer, Subscriber
+from std_msgs.msg import String
+from nav_msgs.msg import Odometry
+from action_msgs.srv import CancelGoal
 
 class ObjectDetection(Node):
-    
+
     def __init__(self, **args):
         super().__init__('object_detection')
-        
         self.target_visible = False
-        
+
+        self.client = ActionClient(self, CancelGoal, 'cancel_goal')
+
         self.subscription = self.create_subscription(
             String,'waypoint_count', self.count_callback, 10)
-
-
-        self.node = rclpy.create_node('goal_cancel_node')
-        self.client = ActionClient(self.node, CancelGoal, 'cancel_goal')
-        cancel_future = action_client.async_cancel_goal(goal_handle)
-
-
+        
         self.pub = self.create_publisher(Twist, "/cmd_vel",qos_profile=rclpy.qos.qos_profile_system_default)   
         self.twist = Twist()
         self.linear_speed = 0.0
@@ -51,24 +42,25 @@ class ObjectDetection(Node):
         except ValueError:
             print("Invalid input. Using default values for linear and angular speeds.")
         self.run(l,a)
-
+        
+        
         self.target_name = 'bottle'
         self.frame_id = 'target'
 
         self.detector = Detector(**args)
-
+        self.goal_msg = None 
         self.bridge = CvBridge()
 
-        self.sub_info = Subscriber(
-            self, CameraInfo, 'camera/aligned_depth_to_color/camera_info')
-        self.sub_color = Subscriber(
-            self, Image, 'camera/color/image_raw')
-        self.sub_depth = Subscriber(
-            self, Image, 'camera/aligned_depth_to_color/image_raw')
         self.ts = ApproximateTimeSynchronizer(
-            [self.sub_info, self.sub_color, self.sub_depth], 10, 0.1)
+            [Subscriber(self, CameraInfo, 'camera/aligned_depth_to_color/camera_info'),
+             Subscriber(self, Image, 'camera/color/image_raw'),
+             Subscriber(self, Image, 'camera/aligned_depth_to_color/image_raw')],
+            slop=0.1, queue_size=10) 
         self.ts.registerCallback(self.images_callback)
+
         self.broadcaster = TransformBroadcaster(self)
+        
+        self.count_value = 0
 
     def count_callback(self, msg):
         count_value = int(msg.data)
@@ -77,20 +69,30 @@ class ObjectDetection(Node):
         self.count_value_list.append(count_value)
         self.count_value_list.pop(0)
 
-    def cancel_goal(future):
-        self.cancel_response = future.result()
-
 
     def run(self, l, a):
         self.twist.linear.x = float(l)
         self.twist.angular.z = float(a)
-
         self.pub.publish(self.twist)
 
+
+
+    async def cancel_navigation_goal(self):
+        goal_handle = await self.client.wait_for_server()
+        cancel_goal_msg = CancelGoal.Goal()
+        future = self.client.async_send_goal(cancel_goal_msg)
+        await future
+        if future.done():
+            cancel_response = future.result()
+            self.get_logger().info(f"Navigation goal cancellation response: {cancel_response}")
+
+
     def images_callback(self, msg_info, msg_color, msg_depth):
+        #self.goal_msg = NavigateToPose.Goal()
+        #self.get_logger().info("Callback called")
         try:
-            img_color = CvBridge().imgmsg_to_cv2(msg_color, 'bgr8')
-            img_depth = CvBridge().imgmsg_to_cv2(msg_depth, 'passthrough')
+            img_color = self.bridge.imgmsg_to_cv2(msg_color, 'bgr8')
+            img_depth = self.bridge.imgmsg_to_cv2(msg_depth, 'passthrough')
         except CvBridgeError as e:
             self.get_logger().warn(str(e))
             return
@@ -101,8 +103,9 @@ class ObjectDetection(Node):
 
         img_color, result = self.detector.detect(img_color)
 
-        #cv2.imshow('color', img_color)
+        cv2.imshow('color', img_color)
 
+        
         target = None
         if self.target_visible == False:
             for r in result:
@@ -111,7 +114,7 @@ class ObjectDetection(Node):
                     break
 
         if target is not None:
-            self.cancel_response
+            self.cancel_navigation_goal()
             u1 = round(target.u1)
             u2 = round(target.u2)
             v1 = round(target.v1)
@@ -121,10 +124,10 @@ class ObjectDetection(Node):
             depth = np.median(img_depth[v1:v2+1, u1:u2+1])
             if depth != 0:
                 z = depth * 1e-3
-                fx = msg_info.k[0]
-                fy = msg_info.k[4]
-                cx = msg_info.k[2]
-                cy = msg_info.k[5]
+                fx = float(msg_info.k[0])
+                fy = float(msg_info.k[4])
+                cx = float(msg_info.k[2])
+                cy = float(msg_info.k[5])
                 x = z / fx * (u - cx)
                 y = z / fy * (v - cy)
                 self.get_logger().info(
@@ -136,8 +139,8 @@ class ObjectDetection(Node):
                 ts.transform.translation.y = y
                 ts.transform.translation.z = z
                 self.broadcaster.sendTransform(ts)
-               
-               #ロボット制御部分
+
+                #ロボット制御部分
                 if ts.transform.translation.x >= 0.03 and ts.transform.translation.z >=0.5:
                     self.twist.linear.x = 0.1
                     self.twist.angular.z = -0.7
@@ -176,6 +179,8 @@ class ObjectDetection(Node):
         elif self.target_visible == True:
             subprocess.Popen(["bash", "move_goal.bash", str(self.count_value_list[-1])])
             self.target_visible = False
+                    
+
 
         img_depth *= 16
         if target is not None:
@@ -183,7 +188,7 @@ class ObjectDetection(Node):
             pt2 = (int(target.u2), int(target.v2))
             cv2.rectangle(img_depth, pt1=pt1, pt2=pt2, color=0xffff)
 
-        #cv2.imshow('depth', img_depth)
+        cv2.imshow('depth', img_depth)
         cv2.waitKey(1)
 
 def main():
@@ -195,3 +200,7 @@ def main():
     except KeyboardInterrupt:
         pass
     rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
